33a1dee6acde52e383230f5a813a5da4c6b5e22d
Michael Antoun <antounmichael@yahoo.com>
2023-11-28 13:30:39 -0800

    tests pass

--- test/test_request_utils.py
+++ test/test_request_utils.py
@@ -8,6 +8,7 @@ from unittest.mock import patch, MagicMock, AsyncMock
 import asyncio
 from aiohttp import ClientSession, ClientConnectorError
 from request_utils import fetch_html, _make_requests, fetch_parallel_requests
+import errno
 
 class TestRequestUtils(unittest.TestCase):
 
@@ -15,14 +16,17 @@ class TestRequestUtils(unittest.TestCase):
         with patch('aiohttp.ClientSession.request', new_callable=AsyncMock) as mock_request:
             mock_request.return_value.status = 200
             mock_request.return_value.json.return_value = {'status': 'OK'}
-            asyncio.run(fetch_html('url', ClientSession()))
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(fetch_html('url', ClientSession()))
             mock_request.assert_called_once_with(method='GET', url='url')
             # Add additional assertions if needed
-    
+
+        
     def test_fetch_html_failure(self):
         with patch('aiohttp.ClientSession.request', new_callable=AsyncMock) as mock_request:
-            mock_request.side_effect = ClientConnectorError()
-            asyncio.run(fetch_html('url', ClientSession()))
+            mock_request.side_effect = ClientConnectorError(os_error=OSError(errno.ENETUNREACH, 'Network is unreachable'), connection_key=None)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(fetch_html('url', ClientSession()))
             mock_request.assert_called_once_with(method='GET', url='url')
             # Add additional assertions if needed
 
