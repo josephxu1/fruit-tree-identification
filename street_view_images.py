5ca32e043c3d84fa57b4de2f6c60e94baedc3240
Michael Antoun <antounmichael@yahoo.com>
2023-12-01 12:29:57 -0800

    added main method, changed download code

--- street_view_images.py
+++ street_view_images.py
@@ -1,5 +1,7 @@
+import asyncio
 import os
 from request_utils import fetch_parallel_requests
+import aiohttp
 
 class StreetViewImageDownloader:
     SIZE = '600x400'
@@ -15,9 +17,10 @@ class StreetViewImageDownloader:
 
     def make_output_dir(self) -> None:
         # create destination folder for downloaded images
+        # if folder already exists, do nothing
         print('Attempting to make output directory')
         try:
-            os.mkdir(self.output_directory)
+            os.makedirs(self.output_directory, exist_ok=True)
             print('Successfully created output directory')
         except OSError as error:
             print(f'Failed with error {error}')
@@ -40,8 +43,11 @@ class StreetViewImageDownloader:
         meta_results = await fetch_parallel_requests(meta_urls)
         valid_addresses = []
         for address, meta_response in zip(addresses, meta_results):
-            if meta_response[0].json()['status'] == 'OK':
+            json_response = await meta_response[0].json()
+            print(json_response)
+            if json_response['status'] == 'OK':
                 valid_addresses.append(address)
+                print(f'Valid metadata at {address}.')
             else:
                 print(f'Invalid metadata at {address}.')
         return valid_addresses
@@ -64,11 +70,44 @@ class StreetViewImageDownloader:
     async def download_images(self, valid_addresses: list[str], size: str = None, fov: int = None) -> None:
         # Execute image requests using request utils
         # download results to output directory
+        if not valid_addresses:
+            print('No valid addresses to download')
+            return
         size = size or self.SIZE
         fov = fov or self.FOV
         self.make_output_dir()
         image_urls = self.generate_download_urls(valid_addresses, size, fov)
-        image_results = await fetch_parallel_requests(image_urls)
-        for address, image in zip(valid_addresses, image_results):
-            with open(os.path.join(self.output_directory, f"{address}.jpg"), 'wb') as file:
-                file.write(image.content)
+
+        async def download_image(url: str, output_path: str) -> None:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    with open(output_path, 'wb') as file:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            file.write(chunk)
+                            print(f'chunk written on {output_path}')
+
+        tasks = []
+        for url, address in zip(image_urls, valid_addresses):
+            output_path = os.path.join(self.output_directory, f"{address}.jpg")
+            task = download_image(url, output_path)
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+async def main():
+    downloader = StreetViewImageDownloader('validation_images')
+    input_path = "./addresses_for_validation_set.txt"
+    with open(input_path, "r") as f:
+        addresses = f.read().splitlines()
+    
+    valid_addresses = await downloader.execute_meta_check(set(addresses))
+
+    await downloader.download_images(valid_addresses)
+
+if __name__ == '__main__':
+    # asyncio.run does not work without this on windows
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
+    
